9970ed8328cb2ee4972fe7782cb98403436d699a
alejoCas91 <161416106+alejoCas91@users.noreply.github.com>
2024-04-29 19:02:11 -0500

    first exercise finished

--- seccion1/ejercicio1.js
+++ seccion1/ejercicio1.js
@@ -17,13 +17,55 @@ function testScope() {
 console.log("Fuera de la función:", globalVariable);
 testScope();
 
-const acceder = prompt("A cuál variable puedes acceder? \n1 = global \n2 = funcion \n3 = bloque")
-if (acceder == 1) {
-  alert("así es, puedes acceder a la variable global en desde cualquier parte del codigo, ya que esta misma está en el top del codigo y afuera de funciones");
-} else{
-    alert("Incorrecto, las variables de bloque y funcion no se pueden acceder porque están fuera de la funcion en la que fueron declaradas, solo pueden ser llamadas dentro de aquella funcion")
+let choose = prompt(
+  "Cuál scope quieres ver? \n1 = global \n2 = function \n3 = block"
+);
+switch (choose) {
+  case "1":
+    globalScope();
+    break;
+  case "2":
+    functionScope();
+  case "3":
+    blockScope();
 }
-
-
-// console.log(blockVariable); //no se puede acceder porque está fuera de la funcion/bloque
-// console.log(functionVariable); //no se puede acceder porque está fuera de la funcion/bloque
+function globalScope() {
+  const acceder = prompt(
+    "A cuál variable puedes acceder de manera global? \n1 = global \n2 = funcion \n3 = bloque"
+  );
+  if (acceder == 1) {
+    alert(
+      "Así es, puedes acceder a la variable global en desde cualquier parte del codigo, ya que esta misma está en el top del codigo y afuera de funciones"
+    );
+  } else {
+    alert(
+      "Incorrecto, las variables de bloque y funcion no se pueden acceder porque están fuera de la funcion en la que fueron declaradas, solo pueden ser llamadas dentro de aquella funcion"
+    );
+  }
+}
+function functionScope() {
+  const acceder = prompt(
+    "A cuáles variables puedes acceder de manera global? \n1 = global \n2 = funcion \n3 = bloque"
+  );
+  if ((acceder == 1) | 2) {
+    alert(
+      "Así es, el scope local o function scope tiene acceso a el scope global y el mismo, osea al scope local"
+    );
+  } else {
+    alert("Incorrecto, el scope local no puede acceder a el scope en bloque");
+  }
+}
+function blockScope() {
+  const acceder = prompt(
+    "A cuál variable puedes acceder de manera block? \n1 = global \n2 = funcion \n3 = bloque \n4 = todas"
+  );
+  if (acceder == 4) {
+    alert(
+      "Así es, las variables en bloque pueden acceder al scope global y al scope de la function"
+    );
+  } else {
+    alert(
+      "correcto e incorrecto, el scope en bloque puede acceder a todos los scopes"
+    );
+  }
+}
